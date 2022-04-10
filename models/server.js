29d7634a4d6cb80fdf7a92836a2b41e916af21af
Jhonnatan Zevallos Contreras <JAZevallosContreras@teco.com.ar>
2022-04-10 19:54:05 -0300

    primer commit

---
+++ models/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const { socketController } = require('../sockets/controller');
+
+class Server {
+
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server); 
+
+        //Middlewares
+        this.middlewares();
+
+        //Routes
+        this.routes();
+
+        this.sockets();
+    }
+
+    middlewares() {
+        //Cors
+        this.app.use(cors());
+
+        //Directorio publico
+        this.app.use(express.static('public'))
+    }
+
+    async routes(){
+        // this.app.use('/api/auth', require('../routes/auth'));
+    }
+
+    sockets() {
+        this.io.on('connection', socketController);
+    }
+
+    listen(){
+        this.server.listen(this.port, () => {
+            console.log(`Servidor escuchando en el puerto ${this.port}`);
+        })
+    }
+}
+
+module.exports = Server;
